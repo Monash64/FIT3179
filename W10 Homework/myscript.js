1c71ba63b43e5ec1538626530485999a38632c0d
Monash64 <111491353+Monash64@users.noreply.github.com>
2022-10-06 15:03:06 +1100

    Add files via upload

---
+++ W10 Homework/myscript.js
@@ -0,0 +1,9 @@
+var vg_1 = "State wheat map.json"; 
+vegaEmbed("#map", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "Country Production.json"; 
+vegaEmbed("#areachart", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
