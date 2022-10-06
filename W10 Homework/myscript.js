480ad2b1812af9f85c80cd88b5fea8b8de735ae4
Monash64 <111491353+Monash64@users.noreply.github.com>
2022-10-06 15:01:50 +1100

    Delete W10 Homework directory

--- W10 Homework/myscript.js
+++
@@ -1,9 +0,0 @@
-var vg_1 = "State wheat map.json"; 
-vegaEmbed("#map", vg_1).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
-
-var vg_2 = "Country Production.json"; 
-vegaEmbed("#map", vg_1).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
